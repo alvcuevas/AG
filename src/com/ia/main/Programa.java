4997705db1e306a91ff07bf1fb70b69ced87e48f
onzinsky <onz99onz9@gmail.com>
2017-04-24 17:09:15 +0200

    Cambios varios
    Ya reconoce ño!!

--- src/com/ia/main/Programa.java
+++ src/com/ia/main/Programa.java
@@ -1,16 +1,28 @@
 package com.ia.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ia.model.Parametros;
 import com.ia.util.Util;
 
 public class Programa {
 	
+	static Parametros parametros ;
+	static List<String> poblacion = new ArrayList<String>();
+	static String target ;
+	static String solucionActual;
+	
 	// Para ir probando el programa
 	public static void main(String[] args) {
-		Util.obtenerParametros(null);
-		Util.leeTarget();
-		Util.generaPoblacion();
-		Util.evaluaFitness();
-		Util.codificaIndividuos();
+		parametros = Util.obtenerParametros(null);
+		target = Util.leeTarget();
+		poblacion = Util.generaPoblacion(target.length(), target);
+		for(int i=0; i<100; i++){
+			solucionActual = Util.evaluaFitness(poblacion, target);
+			if(solucionActual.equals(target)) break;
+		}
+//		Util.codificaIndividuos(target);
 	}
 
 }
--- src/com/ia/util/Util.java
+++ src/com/ia/util/Util.java
@@ -1,5 +1,12 @@
 package com.ia.util;
 
+import static com.ia.util.Constantes.MAX_GENERACIONES;
+import static com.ia.util.Constantes.NUM_GEN_MUESTREO;
+import static com.ia.util.Constantes.NUM_GEN_RESUMEN;
+import static com.ia.util.Constantes.NUM_INDIVIDUOS;
+import static com.ia.util.Constantes.PROB_MUTACION;
+import static com.ia.util.Constantes.RUTA_ARCHIVO_CONFIGURACION;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -9,44 +16,37 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-import static com.ia.util.Constantes.RUTA_ARCHIVO_CONFIGURACION;
-import static com.ia.util.Constantes.MAX_GENERACIONES;
-import static com.ia.util.Constantes.NUM_GEN_MUESTREO;
-import static com.ia.util.Constantes.NUM_GEN_RESUMEN;
-import static com.ia.util.Constantes.NUM_INDIVIDUOS;
-import static com.ia.util.Constantes.PROB_MUTACION;
-
-
 import com.ia.model.Parametros;
 
 public class Util {
 	
-	static Parametros parametros = new Parametros();
 	static Random r = new Random();
-	static List<String> frase = new ArrayList<String>();
-	static List<Character> poblacion = new ArrayList<Character>();
 	
-	public static void main(String[] args) {
-		obtenerParametros(null);
-		leeTarget();
-		generaPoblacion();
-		evaluaFitness();
-		codificaIndividuos();
-    }
+//	public static void main(String[] args) {
+//		obtenerParametros(null);
+//		leeTarget();
+//		generaPoblacion();
+//		evaluaFitness();
+//		codificaIndividuos();
+//    }
 	
 	// Lee la frase inicial por consola y lo almacena en un array
-	public static void leeTarget(){
+	public static String leeTarget(){
 		
+		String target = new String();
 		Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce la frase: ");
  
         if(scanner.hasNextLine())
-        	frase.add(scanner.nextLine().toString());
+        	target = scanner.nextLine().toString();
         
-        for(int i=0; i<frase.size(); i++)
-        	System.out.println("(*)Sentencia target: " + frase.get(i).toString() + " (longitud " + frase.get(i).length() + ")");
+        System.out.println("(*)Sentencia target: " + target.toString() + " (longitud " + target.length() + ")");
+
+        for(int i=0; i<target.length(); i++)
         
-        scanner.close();		
+        scanner.close();	
+        
+        return target;
 	}
 	
 	
@@ -65,6 +65,7 @@ public class Util {
 	// Obtencion de los parametros del archivo configuracion para almacenarlos en la clase Parametros
 	public static Parametros obtenerParametros(String ruta){
 		
+		Parametros parametros = new Parametros();
 		String archivo = leerArchivo(ruta==null||ruta.isEmpty()?RUTA_ARCHIVO_CONFIGURACION:ruta);
 		String[] lineas = archivo.split("\n");
 		
@@ -79,23 +80,18 @@ public class Util {
 				
 				if(linea.startsWith(NUM_INDIVIDUOS)){
 					parametros.setNumIndividuos(Integer.valueOf(valor));
-					//System.out.println(parametros.getNumIndividuos());
 				}
 				if(linea.startsWith(MAX_GENERACIONES)){
 					parametros.setNumMaxGeneraciones(Integer.valueOf(valor));
-					//System.out.println(parametros.getNumMaxGeneraciones());
 				}
 				if(linea.startsWith(NUM_GEN_RESUMEN)){
 					parametros.setNumGenResumen(Integer.valueOf(valor));
-					//System.out.println(parametros.getNumGenResumen());
 				}
 				if(linea.startsWith(NUM_GEN_MUESTREO)){
 					parametros.setNumGenMuestreo(Integer.valueOf(valor));
-					//System.out.println(parametros.getNumGenMuestreo());
 				}
 				if(linea.startsWith(PROB_MUTACION)){
 					parametros.setProbMutacion(Double.valueOf(valor));
-					//System.out.println(parametros.getProbMutacion());
 				}
 			}
 			
@@ -107,59 +103,119 @@ public class Util {
 	}
 	
 	// Genera poblacion de individuos aleatorios y los almacena
-	public static void generaPoblacion(){
+	public static List<String> generaPoblacion(int numIndividuos, String target){
 		
-		int individuos = parametros.getNumIndividuos();
+		List<String> poblacion = new ArrayList<String>();
 		
-		for(int i=0; i<individuos; i++){
-			int c = r.nextInt(26) + (byte)'a';
-			poblacion.add((char)c);
+		for(int i=0; i<numIndividuos; i++){
+			
+			poblacion.add(new String());
+			
+			for(int j=0; j<target.length(); j++){
+				int c = _obtenerRandom();
+				poblacion.set(i, poblacion.get(i) + (char)c);
+			}
 		}
 		
-		System.out.printf("(*)Poblacion aleatoria inicial: ");
+		System.out.printf("(*)Poblacion aleatoria inicial: \n");
 		for(int i=0; i<poblacion.size(); i++)
-			System.out.printf(poblacion.get(i).toString()+", ");	
+			System.out.printf(poblacion.get(i).toString()+", \n");	
 	    
+		return poblacion;
+	}
+	
+	public static int _obtenerRandom(){
+		int c = r.nextInt(27) + (byte)'a';
+		if(c==(byte)'a'+26) c = 32;
+		return c;
+	}
+	
+	public static int _obtenerRandomMax(int max){
+		int c = r.nextInt(max);
+		return c;
 	}
 	
 	
 	// Codifica los individuos de la poblacion para obtener sus cromosomas equivalentes
-	public static void codificaIndividuos(){
+	public static void codificaIndividuos(List<Character> target){
 
 		System.out.printf("\n(*)Cromosomas de cada individuo: ");
-		for(int i=0; i<poblacion.size(); i++){
-			int y = (int)poblacion.get(i);
+		for(int i=0; i<target.size(); i++){
+			int y = (int)target.get(i);
 			System.out.printf(Integer.toBinaryString(y)+", ");
 		}
 			
 	}
 	
 	
-	// Evalua el fitness => número de coincidencias de la poblacion con la frase introducida
-	public static void evaluaFitness(){
+	// Evalua el fitness => nï¿½mero de coincidencias de la poblacion con la frase introducida
+	public static String evaluaFitness(List<String> poblacion, String target){
 		
-		List<Character> coincidencias = new ArrayList<Character>();
-		char[] letrasFrase = frase.toString().toCharArray();
+		List<Integer> numCoincidenciasI = new ArrayList<Integer>();
+		List<Double> listaFitness = new ArrayList<Double>();
+		List<Double> fitnessNormalizado = new ArrayList<Double>();
+		Integer numeroMaximo = 0;
+		Double maximoFitness = new Double(0);
+		Integer indiceMejorFitness = 0;
 		
-		for(int i=0; i<letrasFrase.length; i++){
-			for(int j=0; j<poblacion.size(); j++){
-				if(letrasFrase[i] == poblacion.get(j).charValue()){
-					coincidencias.add(poblacion.get(j).charValue());
+		for(int i=0; i<poblacion.size(); i++){
+			numCoincidenciasI.add(new Integer(0));
+			for(int j=0; j<target.length(); j++){
+			
+				if(poblacion.get(i).charAt(j) == target.charAt(j)){
+					numCoincidenciasI.set(i, numCoincidenciasI.get(i)+1);
 				}
+			}
+			if(numeroMaximo==0 || numeroMaximo<numCoincidenciasI.get(i)) 
+				numeroMaximo = numCoincidenciasI.get(i);
+		}
+		
+		System.out.println(numCoincidenciasI);
+
+		for(int i=0; i<poblacion.size(); i++){
+			listaFitness.add(Math.pow(Math.E, (numCoincidenciasI.get(i)-target.length()))
+					- Math.pow(Math.E, (-target.length())));
+		}
+		
+		System.out.println(listaFitness);
+
+		for(int i=0; i<poblacion.size(); i++){
+			fitnessNormalizado.add(listaFitness.get(i) / 
+					(Math.pow(Math.E, numeroMaximo-target.length() - Math.pow(Math.E, -target.length()))));
+		}
+		
+		System.out.println(fitnessNormalizado);
+		
+		for(int i=0; i<poblacion.size(); i++){
+			if(maximoFitness==new Double(0) || maximoFitness<fitnessNormalizado.get(i)){
+				maximoFitness = fitnessNormalizado.get(i);
+				indiceMejorFitness = i;
 			}
 		}
 		
-		System.out.println("\n(*)Coincidencias target/individuos: " + coincidencias.size());
+		System.out.println(indiceMejorFitness);
 		
-		System.out.printf("(*)Elementos coincidentes: ");
-		for(int i=0; i<coincidencias.size(); i++)
-			System.out.printf(coincidencias.get(i).toString() + ", ");
-
+		String mejorSolucion = poblacion.get(indiceMejorFitness);
+		String nuevaSolucion = new String();
+		
+		for(int i=0; i<target.length(); i++){
+			if(target.charAt(i)!=mejorSolucion.charAt(i)){
+				int c = _obtenerRandom();
+				nuevaSolucion += (char) c;
+			} else {
+				nuevaSolucion += mejorSolucion.charAt(i);
+			}
+		}
+		
+		System.out.println(nuevaSolucion);
+		
+		int nuevaPosicion = _obtenerRandomMax(target.length());
+		
+		poblacion.set(nuevaPosicion, nuevaSolucion);
+		
+		return nuevaSolucion;
 		
 	}
-	
-
-	
 	
 }
 
